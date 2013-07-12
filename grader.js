d9bdde61cc5142addb8e33b7ce31e742046c2d2c
krzysztof.balonek <krzysztof.balonek@gmail.com>
2013-07-12 21:16:03 +0100

    grader modified to download documents given by url

--- grader.js
+++ grader.js
@@ -24,16 +24,42 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require("restler");
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+
+var URL_DEFAULT = "http://whispering-wildwood-1454.herokuapp.com/";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	console.log("%s does not exist. Exiting.", instr);
+	process.exit(1); /* http://nodejs.org/api/process.html#process_process_exit_code*/
     }
     return instr;
+};
+
+var validate = function($, checksArr){
+    var out = {};
+    for(var ii in checksArr) {
+	var present = $(checksArr[ii]).length > 0;
+	out[checksArr[ii]] = present;
+    }
+    return out;
+};
+
+var buildRestFn = function(url, checksfile){
+
+    var getDocument = function(result, response) {
+
+	if (result instanceof Error) {
+	    console.error("%s does not exist. Exiting.", url);
+	    process.exit(1);
+	} else {
+	    logJson(checkHtmlString(result, checksfile));
+	}
+    } ;
+    return getDocument;
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -41,40 +67,59 @@ var cheerioHtmlFile = function(htmlfile) {
 };
 
 var loadChecks = function(checksfile) {
+    console.log("* loadChecks: checksfile="+checksfile);
     return JSON.parse(fs.readFileSync(checksfile));
+};
+
+var checkHtmlString = function(htmlString, checksfile) {
+    $ = cheerio.load(htmlString);
+    var checks = loadChecks(checksfile).sort();
+    return validate($, checks);
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
-    }
-    return out;
+    return validate($, checks);
 };
 
 var clone = function(fn) {
-    // Workaround for commander.js issue.
-    // http://stackoverflow.com/a/6772648
+    /* Workaround for commander.js issue.
+     http://stackoverflow.com/a/6772648 */
     return fn.bind({});
+};
+
+var processUrl = function(url, checksfile) {
+    console.log("* processUrl: "+url+", "+checksfile);
+    var loadAndCheck = buildRestFn(url, checksfile);
+    restler.get(url).on('complete', loadAndCheck);
+    console.log("checked");
+};
+
+var logJson = function(obj) {
+    var outJson = JSON.stringify(obj, null, 4);
+    console.log(outJson);
 };
 
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>',
-		'Path to checks.json', 
+	.option('-c, --checks <check_file>',
+		'Path to checks.json',
 		clone(assertFileExists),
 		CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>',
+	.option('-f, --file <html_file>',
 		'Path to index.html',
 		clone(assertFileExists),
 		HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+	.option('-u, --url <url>',
+		'URL to index.html')
+	.parse(process.argv);
+    if (typeof program.url === "undefined") {
+	logJson(checkHtmlFile(program.file, program.checks));
+    } else {
+	console.log("checking "+program.url);
+	processUrl(program.url, program.checks);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
